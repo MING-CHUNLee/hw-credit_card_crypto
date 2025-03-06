13fd5cd8b15dab8ebaf6d1e3d3998136acbb204e
MING-CHUNLee <mindy80230@gmail.com>
2025-03-06 13:03:20 +0800

    hw:Luhn Algorithm

--- luhn_validator.rb
+++ luhn_validator.rb
@@ -1,4 +1,7 @@
+# Author: Robin Goyal
+# frozen_string_literal: true
 module LuhnValidator
+
   # Validates credit card number using Luhn Algorithm
   # arguments: none
   # assumes: a local String called 'number' exists
@@ -7,5 +10,22 @@ module LuhnValidator
     nums_a = number.to_s.chars.map(&:to_i)
 
     # TODO: use the integers in nums_a to validate its last check digit
+    # last digit is the check digit
+    check_digit = nums_a.pop
+
+    # reverse the array
+    validate_num = nums_a.reverse!
+    # double every second digit
+    validate_num.each_with_index do |num, index|
+      validate_num[index] = transform_digit(num) if index.even?
+    end
+
+    ((validate_num.sum + check_digit) % 10).zero?
+  end
+
+  def transform_digit(num)
+    num *= 2
+    num -= 9 if num > 9
+    num
   end
 end
